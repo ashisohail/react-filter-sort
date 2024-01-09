17293e5f8305446cf0f4c919decd64a8b035dea7
Ayesha <pu_ash@yahoo.com>
2024-01-09 12:46:06 +0000

    Created mock API and created axios request to get data from mock API.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,19 +1,63 @@
 import { useEffect, useState } from "react";
-import products from "../data/data.json";
 import FilterProducts from "./FilterProducts";
 import ProductCard from "./ProductCard";
 import DropdownMenu from "./DropdownMenu";
 import "./Products.css";
 import AddProduct from "./AddProduct";
+import axios from "axios";
 
 function Products() {
+  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState(products);
   const [searchInput, setSearchInput] = useState("");
   const [sortTitle, setSortTitle] = useState("Sort");
 
   useEffect(() => {
+    // Make a GET request to a URL
+    axios
+      .get("http://localhost:3001/recipies")
+      .then((response) => {
+        // handle success
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        // handle error
+        alert(error.message);
+      });
+
+    //async await method
+
+    // const fetchRecepies = async () => {
+    //   try {
+    //     const resp = await fetch("http://localhost:3001/recipies");
+    //     // if (!resp.ok) return;
+    //     setProducts(await resp.json());
+    //   } catch (err) {
+    //     const errorElement = document.getElementById("error");
+    //     errorElement.innerText = "there was an error";
+    //   }
+    // };
+    // fetchRecepies();
+
+    //fetch method
+
+    // function fetchData() {
+    //   fetch("http://localhost:3001/recipies")
+    //     .then((response) => {
+    //       return response.json();
+    //     })
+    //     .then((data) => {
+    //       return setProducts(data);
+    //     })
+    //     .catch((err) => console.log("error", err));
+    // }
+    // fetchData();
+  }, []);
+
+  useEffect(() => {
     if (searchInput === "") {
       setFilteredProducts(products);
+      // filteredProdzucts;
     } else {
       setFilteredProducts(
         filteredProducts.filter(
@@ -27,7 +71,7 @@ function Products() {
         )
       );
     }
-  }, [searchInput]);
+  }, [products, searchInput]);
 
   const handleDropDownChange = (e) => {
     if (e.target.value === "Price Low to High") {
@@ -42,6 +86,7 @@ function Products() {
 
   return (
     <>
+      <div id="error"></div>
       <div className="sortfilter-container">
         <FilterProducts
           setSearchInput={setSearchInput}
